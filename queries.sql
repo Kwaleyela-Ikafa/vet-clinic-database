712537dc6ab3886116f85ee597e848c0054f71fc
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-10-19 11:35:31 +0200

    Update Table

--- queries.sql
+++ queries.sql
@@ -8,3 +8,75 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5 ;
 SELECT * FROM animals WHERE neutered = '1';
 SELECT * FROM animals WHERE name NOT like 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+------
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+
+------
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species is null;
+COMMIT;
+
+------
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+------
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
+------
+SELECT COUNT(name)
+FROM animals;
+
+------
+SELECT COUNT(name)
+FROM animals
+WHERE escape_attempts = 0;
+
+------
+SELECT AVG(weight_kg)
+FROM animals;
+
+------
+SELECT neutered, escape_attempts
+from animals
+WHERE escape_attempts = (
+  SELECT MAX(escape_attempts)
+  from animals
+);
+
+------
+SELECT species,
+MIN(weight_kg),
+MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+------
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
+GROUP BY species;
